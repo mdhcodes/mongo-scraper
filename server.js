5d112edfc35dc540d8f97e612fc14b0a182fbff3
Michelle Hartley <mdhcodes@gmail.com>
2017-05-16 21:14:06 -0400

    Scraping data and logging to the console

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 //================================================
+// Express Server
+//================================================
+var express = require('express');
+
+// Initialize Express
+ var app = express();
+
+//================================================
+// Configure express-handlebars
+//================================================
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//================================================
 // Get HTML from URLs
 //================================================
 var request = require('request');
@@ -8,24 +23,44 @@ var request = require('request');
 //================================================
 var cheerio = require('cheerio');
 
-//================================================
-// Request the HTML body from Hacker News
-//================================================
-request('https://news.ycombinator.com/', function(error, response, html) {
-  // Load the HTML into cheerio and save it to variable
-  var $ = cheerio.load(html);
-  // Store the scraped data in an empty array
-  var result = [];
-  // Select each HTML body instance you want to scrape
-  $('td.title').each(function(i, element) {
-    var link = $(element).children('a:first-child').attr('href');
-    var title = $(element).children().text();
 
-    // Save these results in an object that we'll push into the result array we defined earlier
-    result.push({
-      title: title,
-      link: link
-    });
+// Make public a static dir
+app.use(express.static(process.cwd() + '/public'));
+
+// app.get('/scraped', function(req, res) {
+//   res.redirect('/');
+// });
+
+// A GET request to scrape the NY Times Ceramics and Pottery News website
+ app.get('/scraped', function(req, res) {
+  // Get the HTML body with request
+  request('https://www.nytimes.com/topic/subject/ceramics-and-pottery', function(error, response, html) {
+    // Load the HTML into cheerio and save it to variable
+    var $ = cheerio.load(html);
+    // Store the scraped data in an empty array
+    var result = [];
+    // Select each HTML body instance you want to
+    $('div.story-body').each(function(i, element) {
+      var link = $(element).children('a').attr('href');
+      var title = $(element).find('a').find('.story-meta').find('h2').text();
+      var summary = $(element).find('a').find('.story-meta').find('p').text();
+      var image = $(element).find('a').find('.wide-thumb').find('img').attr('src');
+
+      // Save these results in an object that we'll push into the result array we defined earlier
+      result.push({
+        link: link,
+        title: title,
+        summary: summary,
+        image: image
+        });
+      });
+      console.log(result);
+      res.send('Scrape Complete');
   });
-  console.log(result);
+ });
+
+
+ // Listen on PORT 3000
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
 });
