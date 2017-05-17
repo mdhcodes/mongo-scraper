9b1bfd99421b880511cae8461ae384fa646f54ec
Michelle Hartley <mdhcodes@gmail.com>
2017-05-16 21:55:09 -0400

    Scraped result array displayed in the browser

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var express = require('express');
 
 // Initialize Express
- var app = express();
+var app = express();
 
 //================================================
 // Configure express-handlebars
@@ -27,9 +27,11 @@ var cheerio = require('cheerio');
 // Make public a static dir
 app.use(express.static(process.cwd() + '/public'));
 
-// app.get('/scraped', function(req, res) {
-//   res.redirect('/');
-// });
+
+// If a user requests this '/' route, they will be redirected to the '/scraped' route.
+app.get('/', function(req, res) {
+ res.redirect('/scraped');
+});
 
 // A GET request to scrape the NY Times Ceramics and Pottery News website
  app.get('/scraped', function(req, res) {
@@ -54,8 +56,35 @@ app.use(express.static(process.cwd() + '/public'));
         image: image
         });
       });
-      console.log(result);
       res.send('Scrape Complete');
+  });
+ });
+
+
+// A GET request to scrape the NY Times Ceramics and Pottery News website
+ app.get('/all', function(req, res) {
+  // Get the HTML body with request
+  request('https://www.nytimes.com/topic/subject/ceramics-and-pottery', function(error, response, html) {
+    // Load the HTML into cheerio and save it to variable
+    var $ = cheerio.load(html);
+    // Store the scraped data in an empty array
+    var result = [];
+    // Select each HTML body instance you want to
+    $('div.story-body').each(function(i, element) {
+      var link = $(element).children('a').attr('href');
+      var title = $(element).find('a').find('.story-meta').find('h2').text();
+      var summary = $(element).find('a').find('.story-meta').find('p').text();
+      var image = $(element).find('a').find('.wide-thumb').find('img').attr('src');
+
+      // Save these results in an object that we'll push into the result array we defined earlier
+      result.push({
+        link: link,
+        title: title,
+        summary: summary,
+        image: image
+        });
+      });
+      res.send(result);
   });
  });
 
